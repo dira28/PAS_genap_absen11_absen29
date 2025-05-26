f69e0bc8dd0cc56c60f33be03bd0425c014b1416
Hiyooooo <hiyokiou@gmail.com>
2025-05-26 09:00:46 +0700

    menambahkan codingan di MatchResponse Dan Match

---
+++ app/src/main/java/com/example/pas_genap_absen11_absen29/Match.java
@@ -0,0 +1,48 @@
+package com.example.pas_genap_absen11_absen29;
+
+public class Match {
+    private String strHomeTeam;
+
+    private String strAwayTeam;
+
+    private String strEvent;
+
+    private String dateEvent;
+
+    private String strHomeTeamBadge;
+
+    private String strAwayTeamBadge;
+
+    private String intHomeScore;
+
+    private String intAwayScore;
+
+    public  String getStrHomeTeam(){
+        return strHomeTeam;
+    }
+
+    public String getStrAwayTeam(){
+        return strAwayTeam;
+    }
+
+    public String getStrEvent() {
+        return strEvent;
+    }
+
+    public String getDateEvent() {
+        return dateEvent;
+    }
+    public String getStrHomeTeamBadge(){
+        return strHomeTeamBadge;
+    }
+    public String getStrAwayTeamBadge(){
+        return strAwayTeamBadge;
+    }
+    public String getIntHomeScore(){
+        return intHomeScore;
+    }
+    public String getIntAwayScore(){
+        return intAwayScore;
+    }
+
+}
--- app/src/main/java/com/example/pas_genap_absen11_absen29/MatchResponse.java
+++ app/src/main/java/com/example/pas_genap_absen11_absen29/MatchResponse.java
@@ -1,4 +1,13 @@
 package com.example.pas_genap_absen11_absen29;
 
+import com.google.gson.annotations.SerializedName;
+
+import  java.util.List;
 public class MatchResponse {
+    @SerializedName("results")
+    private List<Match> events;
+
+    public List<Match> getEvents() {
+        return events;
+    }
 }
